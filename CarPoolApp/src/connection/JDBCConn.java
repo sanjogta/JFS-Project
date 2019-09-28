78966da49bd5a0e44441857a6f671dabb7c22b06
sanjogta <46321908+sanjogta@users.noreply.github.com>
2019-09-28 20:40:28 +0530

    Update JDBCConn.java

--- CarPoolApp/src/connection/JDBCConn.java
+++ CarPoolApp/src/connection/JDBCConn.java
@@ -8,9 +8,16 @@ import java.sql.Statement;
 public class JDBCConn {
   public static void main(String[] args) throws ClassNotFoundException, SQLException
   {
-	  Class.forName("com.mysql.jdbc.Driver");
+	  //Step 1: Register the JDBC driver.
+	  Class.forName("com.mysql.jdbc.Driver"); 
+	  
+	  //Step 2: Create a connection with Mysql.
+	  //Here the first String parameter is the database name, second String parameter is the username and last
+	  // one is the password.
 	  Connection con = DriverManager.getConnection("jdbc:mysql://localhost/my_first_schema","root","sanju123");
 	  System.out.println("Connection established successfully");
+	  
+	  //Step 3: Close the connection.
 	  con.close();
 	  System.out.println("Connection closed");
   }
