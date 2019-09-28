1ca455ab438aec50c22f35fe90a7515384a6714b
sanjogta <46321908+sanjogta@users.noreply.github.com>
2019-09-28 20:54:02 +0530

    Update InsertData.java

--- CarPoolApp/src/insert/InsertData.java
+++ CarPoolApp/src/insert/InsertData.java
@@ -8,16 +8,8 @@ import java.sql.Statement;
 public class InsertData {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		 Class.forName("oracle.jdbc.driver.OracleDriver");
-		  System.out.println("Driver Successfully Registered");
-		  Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","SYSTEM","sanju123");
-		  System.out.println("Successfully Connected to Oracle"); 
-		  
-	}
-	
-	public static void insertValues(Connection con) throws SQLException
-	{
-		Statement stmt = con.createStatement();
-		stmt.executeUpdate("");
+		String url = "C:\\xampp\\htdocs\\JFS Pro\\insert.php";
+		File htmlFile = new File(url);
+		Desktop.getDesktop().browse(htmlFile.toURI());  
 	}
 }
