445c15c452c5754d4d4d5a0e775aa02d15b89953
sanjogta <46321908+sanjogta@users.noreply.github.com>
2019-09-28 20:48:25 +0530

    Update CreateTab.java

--- CarPoolApp/src/createTable/CreateTab.java
+++ CarPoolApp/src/createTable/CreateTab.java
@@ -9,16 +9,22 @@ public class CreateTab {
   public static void main(String[] args) throws ClassNotFoundException, SQLException
   {
 	
-	Class.forName("com.mysql.jdbc.Driver");
-	Connection con = DriverManager.getConnection("jdbc:mysql://localhost/my_first_schema","root","sanju123");
-	System.out.println("Connection established successfully");
+     //Step 1: Register the JDBC driver.
+     Class.forName("com.mysql.jdbc.Driver"); 
+	  
+     //Step 2: Create a connection with Mysql.
+     Connection con = DriverManager.getConnection("jdbc:mysql://localhost/my_first_schema","root","sanju123");
+     System.out.println("Connection established successfully");
 	
-	final String CREATE_EMP="CREATE TABLE EMP_DETAILS (EMPID VARCHAR(6),NAME VARCHAR(50)NOT NULL,CONTACT INTEGER(10) NOT NULL,PASSWORD VARCHAR(8)NOT NULL,DT DATE NOT NULL, PRIMARY KEY (EMPID))";
-    Statement stmt = con.createStatement();
-    stmt.executeUpdate(CREATE_EMP);
+     final String CREATE_EMP="CREATE TABLE EMP_DETAILS (EMPID VARCHAR(6),NAME VARCHAR(50)NOT NULL,CONTACT INTEGER(10) NOT NULL,PASSWORD VARCHAR(8)NOT NULL,DT DATE NOT NULL, PRIMARY KEY (EMPID))";
+     //The createStatement() method of Connection interface is used to create statement. 
+     Statement stmt = con.createStatement();
+	  
+     //The object of statement is responsible to execute queries with the database.	  
+     stmt.executeUpdate(CREATE_EMP);
      	  
-	System.out.println("Created table in given database...");
-	con.close();
-	System.out.println("Connection closed");
+     System.out.println("Created table in given database...");
+     con.close();
+     System.out.println("Connection closed");
   }
 }
